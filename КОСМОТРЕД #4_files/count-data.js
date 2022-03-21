a496b60a79ff3bd9f003d8fd5b402caac044b8f3
Gamanga <79852080+Gamanga@users.noreply.github.com>
2022-03-21 13:34:30 +0200

    Delete КОСМОТРЕД #4_files directory

--- КОСМОТРЕД #4_files/count-data.js
+++
@@ -1,5 +0,0 @@
-var DISQUSWIDGETS;
-
-if (typeof DISQUSWIDGETS != 'undefined') {
-    DISQUSWIDGETS.displayCount({"text":{"and":"\u0438","comments":{"zero":"","multiple":"{num}","one":"1"}},"counts":[]});
-}
