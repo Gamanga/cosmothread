13a96cb3e8ad224239e5191fe8dd72578fa4d5ec
Gamanga <stsyrfa@gmail.com>
2022-10-10 18:57:53 +0300

    +8

---
+++ threads/Легитимный космотред_files/count-data.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"text":{"and":"\u0438","comments":{"zero":"","multiple":"{num}","one":"1"}},"counts":[]});
+}
